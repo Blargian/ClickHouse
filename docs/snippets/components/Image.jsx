f81aa58da072998e3c39ee74e1877b33665dc4ed
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2026-07-13 13:22:09 +0200

    Fix ClickStack docs routing and image sizing

--- docs/snippets/components/Image.jsx
+++ docs/snippets/components/Image.jsx
@@ -1,7 +1,17 @@
-export const Image = ({ img, alt, size }) => {
+export const Image = ({ img, alt, size = "lg" }) => {
+  const width = {
+    sm: "300px",
+    md: "600px",
+    lg: "100%",
+  }[size] ?? "100%";
+
   return (
     <Frame>
-      <img src={img} alt={alt} />
+      <img
+        src={img}
+        alt={alt}
+        style={{ width, maxWidth: "100%", height: "auto", marginInline: "auto" }}
+      />
     </Frame>
   );
 };
